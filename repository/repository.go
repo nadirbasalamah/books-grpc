fcefcfee7162ec3ea9e9fa84341ac53b56902c9d
nadirbasalamah <nadir008basalamah@gmail.com>
2021-08-10 16:43:09 +0700

    feat(repo): Implement all features in repo using DB. Mengimplementasikan semua fungsi di repo menggunakan DB.

--- repository/repository.go
+++ repository/repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/google/uuid"
@@ -33,41 +34,92 @@ func AddBook(bookData model.Book) model.Book {
 
 // GetBook untuk mendapatkan data buku berdasarkan id
 func GetBook(bookId string) (int, model.Book) {
-	for index, v := range storage {
-		if v.Id == bookId {
-			return index, v
+	var book model.Book = model.Book{}
+
+	row, err := database.DB.Query("SELECT * FROM books WHERE id = ?", bookId)
+
+	if err != nil {
+		log.Fatalf("Data cannot be retrieved: %v", err)
+		return 0, model.Book{}
+	}
+
+	defer row.Close()
+
+	for row.Next() {
+		switch err := row.Scan(&book.Id, &book.Title, &book.Author, &book.IsRead); err {
+		case sql.ErrNoRows:
+			log.Printf("Data not found: %v", err)
+			return 0, model.Book{}
+		case nil:
+			log.Println(book)
+		default:
+			log.Printf("Data cannot be retrieved: %v", err)
+			return 0, model.Book{}
 		}
 	}
-	return 0, model.Book{}
+
+	return 1, book
+
 }
 
 // GetBooks untuk mendapatkan seluruh data buku
 func GetBooks() []model.Book {
-	return storage
+	rows, err := database.DB.Query("SELECT * FROM books")
+
+	if err != nil {
+		log.Fatalf("Data cannot be retrieved: %v", err)
+		return []model.Book{}
+	}
+
+	defer rows.Close()
+
+	var books []model.Book = []model.Book{}
+
+	for rows.Next() {
+		var book model.Book = model.Book{}
+
+		err := rows.Scan(&book.Id, &book.Title, &book.Author, &book.IsRead)
+
+		if err != nil {
+			log.Printf("Data cannot be retrieved: %v", err)
+			return []model.Book{}
+		}
+
+		books = append(books, book)
+	}
+
+	if len(books) == 0 {
+		log.Println("Books data not found")
+	}
+
+	return books
+
 }
 
 // UpdateBook untuk mengedit data buku
 func UpdateBook(bookData model.Book, id string) model.Book {
-	index, book := GetBook(id)
+	_, err := database.DB.Query("UPDATE books SET title=?, author=?, is_read=? WHERE id=?",
+		bookData.Title,
+		bookData.Author,
+		bookData.IsRead,
+		bookData.Id,
+	)
 
-	book.Title = bookData.Title
-	book.Author = bookData.Author
-	book.IsRead = bookData.IsRead
+	if err != nil {
+		log.Fatalf("Update data failed: %v", err)
+		return model.Book{}
+	}
 
-	storage[index] = book
+	return bookData
 
-	return book
 }
 
 // DeleteBook untuk menghapus data buku
 func DeleteBook(id string) bool {
-	var afterDeleted []model.Book = []model.Book{}
-	for _, v := range storage {
-		if id != v.Id {
-			afterDeleted = append(afterDeleted, v)
-		}
+	_, err := database.DB.Query("DELETE FROM books WHERE id=?", id)
+	if err != nil {
+		log.Fatalf("Delete data failed: %v", err)
+		return false
 	}
-
-	storage = afterDeleted
 	return true
 }
